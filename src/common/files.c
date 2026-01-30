6614021d641dd9dee5ba799392d0e16d7ec4f34f
PorrOwO <andrea.dalcin04@gmail.com>
2026-01-30 14:01:23 +0100

    Update file component with better error handling

--- src/common/files.c
+++ src/common/files.c
@@ -1,22 +1,55 @@
 #include "files.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-i32 file_read_buffer(char* buffer, const char* fpath, u32 max_buffer_len){
+IOStatus file_read_buffer(char* buffer, const char* fpath, u32 max_buffer_len){
     FILE *fp = fopen(fpath, "r");
     if (fp == NULL) {
-        fprintf(stderr, "Error: Could not open file '%s'\n", fpath);
         buffer[0] = '\0';
-        return -1;
+        return IO_ERROR_OPEN;
     }
     
     size_t newLen = fread(buffer, sizeof(char), max_buffer_len - 1, fp);
     if ( ferror( fp ) != 0 ) {
-        fprintf(stderr, "Error reading file '%s'\n", fpath);
         fclose(fp);
         buffer[0] = '\0';
-        return -1;
+        return IO_ERROR_READ;
     }
     
     buffer[newLen] = '\0';
     fclose(fp);
-    return 0;
+    return IO_SUCCESS;
+}
+
+
+IOStatus file_read_all(string_t* buffer, const char* fpath) {
+    if (!buffer) return IO_ERROR_MEMORY;
+
+    FILE* fp = fopen(fpath, "r");
+    if(fp == NULL) {
+        return IO_ERROR_OPEN;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    u32 length = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    if (length == 0) {
+        fclose(fp);
+        return IO_ERROR_EMPTY;
+    }
+
+    buffer->data = (char*)malloc((length + 1) * sizeof(char));
+    if(!buffer->data) {
+        fclose(fp);
+        return IO_ERROR_MEMORY;
+    }
+
+    size_t read_size = fread(buffer->data, 1, length, fp);
+    buffer->data[read_size] = '\0';
+    buffer->size = read_size;
+    if (read_size != length) {
+        return IO_ERROR_READ;
+    }
+
+    return IO_SUCCESS;
 }
--- src/common/files.h
+++ src/common/files.h
@@ -2,5 +2,26 @@
 #include <stdio.h>
 #include "common/defines.h"
 
-i32 file_read_buffer(char* buffer, const char* fpath, u32 max_buffer_len);
+typedef enum {
+    IO_SUCCESS      =  0,
+    IO_ERROR_OPEN   = -1,
+    IO_ERROR_READ   = -2,
+    IO_ERROR_MEMORY = -3,
+    IO_ERROR_EMPTY  = -4,
+} IOStatus;
 
+// If 'call' is not SUCCESS, print error and return the error code.
+#define IO_CHECK(call) \
+    do { \
+        IOStatus res = (call); \
+        if (res != IO_SUCCESS) { \
+            fprintf(stderr, "[IO ERROR] Code %d at %s:%d\n", res, __FILE__, __LINE__); \
+            return res; \
+        } \
+    } while (0)
+
+/* read content of a file and save it in a fixed length buffer */
+IOStatus file_read_buffer(char* buffer, const char* fpath, u32 max_buffer_len);
+
+/* read the whole content of a file and return a new string_t with the content */
+IOStatus file_read_all(string_t* buffer, const char* fpath);
